cadf8e7c9d9d0e0e131948cd0cf86f06a92ed22d
sassyDave <david@timecodelab.com>
2018-03-24 11:03:57 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'src/scripts/app.js'),
+    path.join(__dirname, 'src/styles/main.scss')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    filename: 'plebei.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/
+      },
+      {
+        enforce: 'pre',
+        test: /\.scss$/,
+        loader: 'import-glob-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "sass-loader" // compiles Sass to CSS
+          }],
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {}
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'main.css',
+      allChunks: true
+    }),
+    new CopyWebpackPlugin([
+      { from: 'manifest.json' },
+      { from: 'index.html', cache: true }
+    ])
+  ]
+};
