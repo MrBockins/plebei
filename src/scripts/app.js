89c6b97b16ff22e783d1afb9f5e390cbc48af43d
sassyDave <david@timecodelab.com>
2018-03-24 11:31:43 -0400

    fetching data

---
+++ src/scripts/app.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+class Plebei {
+  constructor () {
+    this.bindEvents();
+    // this.fetchStuff('1091185');
+  }
+  bindEvents() {
+    let fetchBtn = document.querySelectorAll('.js-fetch')[0]
+    fetchBtn.addEventListener('click', this.fetchStuff);
+  }
+  fetchStuff() {
+    let id = 1091185;
+    axios({
+      method: 'get',
+      url: `http://services.radio-canada.ca/neuro/v1/news-stories/${id}`
+    })
+    .then(function (response) {
+      console.log(response.data.body.html);
+    });
+  }
+}
+
+new Plebei();
