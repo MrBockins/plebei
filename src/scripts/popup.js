188c2ab95d38d3dd417e75ab496ba5f41f8029b5
sassyDave <david@timecodelab.com>
2018-03-24 19:07:05 -0400

    events communication between popup and extension

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 class App {
   constructor() {
-    this.mediaId = document.querySelectorAll('meta[name="rc.idMedia"]')[0].content
+    this.mediaId = document.querySelectorAll('meta[name="rc.idMedia"]')[0].content;
+    this.elementContext = undefined;
+
+    this.port = chrome.runtime.connect('ndpiclcbelbammppjdcngmapkndphmmm');
     this._injectElement();
   }
   _injectElement() {
@@ -11,14 +14,36 @@ class App {
       url: chrome.extension.getURL("/templates.html")
     }).then(response => {
       const parser = new DOMParser();
-      const template = parser.parseFromString(response.data, "application/xml").querySelector("#templates-popup").innerHTML;
       const parent = document.querySelectorAll(".header-audio-controller")[0];
+      
+      const template = parser.parseFromString(response.data, "application/xml").querySelector("#templates-popup").innerHTML;
       parent.insertAdjacentHTML("afterbegin", template)
       
+      this.elementContext = parent.querySelector('#plebei-popup');
+
       this._bindEvents();
     });
   }
   _bindEvents() {
+
+    chrome.runtime.onMessage.addListener(
+      function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+          "from a content script:" + sender.tab.url :
+          "from the extension");
+        if (request.greeting == "hello widget"){
+          sendResponse({ farewell: "goodbye 123" });
+          console.log('from addlistener')
+        }
+      });
+
+
+    chrome.runtime.sendMessage({ greeting: 123 }, function (response) {
+      console.log('---POPUP--');
+      console.log(response.farewell);
+      console.log('-----');
+    });
+
     console.log('----------');
     console.log(this.mediaId);
     console.log('----------');
--- src/scripts/widget.js
+++ src/scripts/widget.js
@@ -3,9 +3,25 @@ import axios from "axios";
 class Widget {
   constructor () {
     this.bindEvents();
+    this.port = chrome.runtime.connect('ndpiclcbelbammppjdcngmapkndphmmm');
   }
   bindEvents() {
-    
+    chrome.runtime.onMessage.addListener(
+      function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+          "from a content script:" + sender.tab.url :
+          "from the extension");
+        if (request.greeting == 123)
+          sendResponse({ farewell: (request.greeting*2) });
+      });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello widget" }, function (response) {
+        console.log('---WIDGET--');
+        console.log(response);
+        console.log('-----');
+      });
+    });
   }
 }
 
