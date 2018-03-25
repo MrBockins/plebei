b97a6c8b0a3e97b98c1c2bc221d894832df67fe0
sassyDave <david@timecodelab.com>
2018-03-25 10:29:00 -0400

    icon for extension

--- dist/popup.js
+++ dist/popup.js
@@ -958,36 +958,29 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 class App {
   constructor() {
     this.mediaId = document.querySelectorAll('meta[name="rc.idMedia"]')[0].content;
-
     this.elementContext = undefined;
 
     this._injectElement();
   }
 
   _getNews(id) {
-    console.log('Quering newId => ' + id);
     __WEBPACK_IMPORTED_MODULE_0_axios___default()({
       method: 'get',
       url: `http://services.radio-canada.ca/neuro/v1/news-stories/${id}`
     }).then(function (response) {
       let responseHtml = response.data.body.html;
-
-      console.log("data", responseHtml); //)
       const parser = new DOMParser();
 
       let flufflyfied = parser.parseFromString(responseHtml, "text/html");
 
       let elementsArray = flufflyfied.getElementsByTagName("p");
       let elementsInner = document.querySelectorAll('.document-body')[0].getElementsByTagName("p");
-      console.log("inner " + elementsArray.length + ", " + elementsInner.length);
+
       for (var index = 0; index < elementsArray.length; index++) {
         const line = elementsArray[index].innerText;
         let parsedLine = line + " Sua poudre!!!"; //ICI remplacer par code ALEX...
         elementsInner[index].innerText = parsedLine;
       }
-
-      console.log("flufflyfied => ", flufflyfied);
-      //document.querySelectorAll('.document-body')[0].innerHTML = flufflyfied.querySelector('body').innerHTML;
     });
   }
 
@@ -1005,7 +998,6 @@ class App {
       this.elementContext = parent.querySelector('#plebei-popup');
 
       this._getNews(this.mediaId);
-      //this._bindEvents();
     });
   }
   _bindEvents() {}
